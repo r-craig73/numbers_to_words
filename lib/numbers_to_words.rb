abdb46acb80a6005853388f0505f2ad4a6b84efc
kimberly huynh and Ron Craig <ron@>
2018-02-06 15:05:24 -0800

    initial commit

---
+++ lib/numbers_to_words.rb
@@ -0,0 +1,18 @@
+class Numbers
+  def iniialize(number)
+    @number = number
+
+  end
+
+def words
+
+#add Hash here
+words = Hash.new()
+words = {0 => "zero", 1 => "one"}
+
+
+
+
+
+
+end
---
+++ spec/numbers_to_words_spec.rb
@@ -0,0 +1,13 @@
+require('rspec')
+require('pry')
+require('numbers_to_words')
+
+describe('#number_to_words') do
+  it ("returns the word zero from the number 0") do
+    number = Numbers.new(0)
+    #
+  end
+
+
+
+end
