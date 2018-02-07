5cf5d6bc09e32befef39fb0acfae99cba5f4851c
kimberly huynh and Ron Craig <ron@>
2018-02-06 16:46:00 -0800

    attempt second spec, but more work is needed

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -5,16 +5,21 @@ class Counter
   end
 
   def words
-  words = { 0 => "zero" }
+    words = {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
 
-  # output = ""
-  new_number = @input
+    # output = ""
+    new_number = @input
+    x = 0
     if new_number == 0
-      words.fetch(0)
-#binding.pry
+        words.fetch(0)
+    # else
+    #   low_numbers = (1..9)
+    #   low_numbers.each() do |number|
+    #     if new_number == low_numbers
+    #     words.fetch(number)
+    #     end
     end
-    # output
 
   end
-# binding.pry
+#binding.pry
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -3,9 +3,12 @@ require('pry')
 require('numbers_to_words')
 
 describe('#numbers_to_words') do
-  it("returns something that should work") do
+  it("returns 'zero' when the number is 0") do
     input = Counter.new(0)
     expect(input.words()).to(eq("zero"))
   end
-
+  # it("returns 'two' when the number is 2") do
+  #   input = Counter.new(2)
+  #   expect(input.words()).to(eq("two"))
+  # end
 end
