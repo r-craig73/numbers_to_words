5ffe97ec9359ce99698b3020db8881c8c722bc81
Ron Craig <ron.craig@comcast.net>
2018-09-17 20:46:41 -0700

    add three_digits hash, reduce hash lines for single-ten-two digits

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -7,40 +7,25 @@ class Counter
 
   def words
     single_digits = {
-      0 => 'zero',
-      1 => 'one',
-      2 => 'two',
-      3 => 'three',
-      4 => 'four',
-      5 => 'five',
-      6 => 'six',
-      7 => 'seven',
-      8 => 'eight',
-      9 => 'nine'
+      0 => 'zero', 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four',
+      5 => 'five', 6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine'
     }
 
     ten_digits = {
-      11 => 'eleven',
-      12 => 'twelve',
-      13 => 'thirteen',
-      14 => 'fourteen',
-      15 => 'fifteen',
-      16 => 'sixteen',
-      17 => 'seventeen',
-      18 => 'eighteen',
+      11 => 'eleven', 12 => 'twelve', 13 => 'thirteen', 14 => 'fourteen',
+      15 => 'fifteen', 16 => 'sixteen', 17 => 'seventeen', 18 => 'eighteen',
       19 => 'nineteen'
     }
 
     two_digits = {
-      10 => 'ten',
-      20 => 'twenty',
-      30 => 'thirty',
-      40 => 'forty',
-      50 => 'fifty',
-      60 => 'sixty',
-      70 => 'seventy',
-      80 => 'eighty',
-      90 => 'ninety'
+      10 => 'ten', 20 => 'twenty', 30 => 'thirty', 40 => 'forty', 50 => 'fifty',
+      60 => 'sixty', 70 => 'seventy', 80 => 'eighty', 90 => 'ninety'
+    }
+
+    three_digits = {
+      100 => 'one hundred', 200 => 'two hundred', 300 => 'three hundred',
+      400 => 'four hundred', 500 => 'five hundred', 600 => 'six hundred',
+      700 => 'seven hundred', 800 => 'eight hundred', 900 => 'nine hundred'
     }
 
     new_number = @input
@@ -65,5 +50,9 @@ class Counter
         two_digits.fetch(tens) + ' ' + single_digits.fetch(ones)
       end
     end
+
+
+
+
   end
 end
