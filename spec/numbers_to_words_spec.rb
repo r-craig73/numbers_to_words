171292da9934f8e1114d7fbf63158b9c3d5c3968
Ron Craig <ron.craig@comcast.net>
2018-09-17 19:16:06 -0700

    add new specs, 5 out 5 specs are passing (0->99)

--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -2,13 +2,29 @@ require('rspec')
 require('pry')
 require('numbers_to_words')
 
-describe('#numbers_to_words') do
-  it("returns 'zero' when the number is 0") do
+describe('words') do
+  it('returns string "zero" when the number is 0') do
     input = Counter.new(0)
-    expect(input.words()).to(eq("zero"))
+    expect(input.words).to(eq('zero'))
   end
-  # it("returns 'two' when the number is 2") do
-  #   input = Counter.new(2)
-  #   expect(input.words()).to(eq("two"))
-  # end
+
+  it('returns "two" when the number is 2') do
+    input = Counter.new(2)
+    expect(input.words).to(eq('two'))
+  end
+
+  it('returns "sixteen" when the number is 16') do
+    input = Counter.new(16)
+    expect(input.words).to(eq('sixteen'))
+  end
+
+  it('returns "thirty" when the number is 30') do
+    input = Counter.new(30)
+    expect(input.words).to(eq('thirty'))
+  end
+
+  it('returns "ninety eight" when the number is 99') do
+    input = Counter.new(99)
+    expect(input.words).to(eq('ninety nine'))
+  end
 end
